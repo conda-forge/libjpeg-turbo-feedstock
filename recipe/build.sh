6f2a084f4323f0d40eaa85623026b302fa937d52
santi <sdvillal@gmail.com>
2017-10-03 17:08:08 +0200

    add fPIC

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,10 +2,12 @@
 
 autoreconf -fiv
 
+# Set here any HW specific flags (usually at CFLAGS)
 ./configure --prefix=$PREFIX \
     --enable-shared=yes \
     --enable-static=yes \
     --with-jpeg8 \
+    CFLAGS="-O3 -fPIC" \
     NASM=yasm
 make
 make check
