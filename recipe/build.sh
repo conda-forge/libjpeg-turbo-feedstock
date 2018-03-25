07883aa3d763d31d4af3422ae80aa0debf6f6960
Filipe Fernandes <ocefpaf@gmail.com>
2018-03-25 17:35:32 -0300

    use -DCMAKE_ASM_NASM_COMPILER=yasm

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,10 +5,10 @@ mkdir build_libjpeg && cd  build_libjpeg
 cmake -D CMAKE_INSTALL_PREFIX=$PREFIX \
       -D CMAKE_BUILD_TYPE=Release \
       -D ENABLE_STATIC=1 \
-	  -D ENABLE_SHARED=1 \
-	  -D WITH_JPEG8=1 \
-	  -D NASM=yasm \
-	  $SRC_DIR
+      -D ENABLE_SHARED=1 \
+      -D WITH_JPEG8=1 \
+      -D CMAKE_ASM_NASM_COMPILER=yasm \
+      $SRC_DIR
 
 make -j$CPU_COUNT
 ctest
