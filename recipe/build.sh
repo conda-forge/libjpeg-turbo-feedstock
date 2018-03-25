354b2400dbd45e206c8e268a71f5595edbb664ff
Filipe Fernandes <ocefpaf@gmail.com>
2018-03-25 18:09:06 -0300

    send it to lib and not lib64

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,7 @@
 mkdir build_libjpeg && cd  build_libjpeg
 
 cmake -D CMAKE_INSTALL_PREFIX=$PREFIX \
+      -D CMAKE_INSTALL_LIBDIR="$PREFIX/lib" \
       -D CMAKE_BUILD_TYPE=Release \
       -D ENABLE_STATIC=1 \
       -D ENABLE_SHARED=1 \
