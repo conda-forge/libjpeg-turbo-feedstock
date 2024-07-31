00119000e9f3e068cc19927b47f4e3100fde6d68
Daniel Ching <carterbox@users.noreply.github.com>
2024-07-30 22:25:39 -0500

    BLD: Use new conda-build 24.7 API to split packages

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,10 +1,9 @@
 :: Build step
-mkdir "%SRC_DIR%\stage"
 mkdir build_libjpeg
 cd  build_libjpeg
 
 cmake -G "NMake Makefiles" ^
-	-D CMAKE_INSTALL_PREFIX="%SRC_DIR%\stage" ^
+	-D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
 	-D CMAKE_BUILD_TYPE=Release ^
 	-D ENABLE_STATIC=0 ^
 	-D ENABLE_SHARED=1 ^
@@ -19,10 +18,3 @@ if errorlevel 1 exit 1
 :: Install step
 nmake install
 if errorlevel 1 exit 1
-
-setlocal EnableExtensions ENABLEDELAYEDEXPANSION
-for %%f in ( "%SRC_DIR%\stage\lib\pkgconfig\*.pc" ) do (
-    sed -i.bak "s,prefix=.*,prefix=/opt/anaconda1anaconda2anaconda3/Library,g" %%f
-    del %%f.bak
-)
-endlocal
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-mkdir "$SRC_DIR/stage"
-
 mkdir build_libjpeg && cd  build_libjpeg
 
-cmake ${CMAKE_ARGS} -D CMAKE_INSTALL_PREFIX="$SRC_DIR/stage" \
-      -D CMAKE_INSTALL_LIBDIR="$SRC_DIR/stage/lib" \
+cmake ${CMAKE_ARGS} -D CMAKE_INSTALL_PREFIX=$PREFIX \
+      -D CMAKE_INSTALL_LIBDIR="$PREFIX/lib" \
       -D CMAKE_BUILD_TYPE=Release \
       -D ENABLE_STATIC=0 \
       -D ENABLE_SHARED=1 \
@@ -20,7 +18,4 @@ fi
 make install -j$CPU_COUNT
 
 # We can remove this when we start using the new conda-build.
-find $SRC_DIR/stage -name '*.la' -delete
-
-sed -i.bak "s,$SRC_DIR/stage,/opt/anaconda1anaconda2anaconda3,g" $SRC_DIR/stage/lib/pkgconfig/*.pc
-rm $SRC_DIR/stage/lib/pkgconfig/*.bak
+find $PREFIX -name '*.la' -delete
